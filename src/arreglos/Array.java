ae9b1e20a93af7db2743ea0072233ecaa0b61e6a
Erlinda <'erlindachambimanzanoerlinda@gamil.com'>
2020-03-04 14:44:29 -0400

    ejemplo arreglos

---
+++ src/arreglos/Array.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos;
+
+/**
+ *
+ * @author chamb
+ */
+public class Array {
+    public static void main(String[] args) {
+        int array[] ={1,2,3,4,5,6,7,8,9};
+        
+        for(int i=0; i<array.length; i++){
+            System.out.println(array[i]);
+        }
+    }
+}

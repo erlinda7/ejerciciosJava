d96dc187d87b86b2b88c3c1ff5d02bd3a9e16d4b
Erlinda <'erlindachambimanzanoerlinda@gamil.com'>
2020-03-05 08:21:29 -0400

    ejemplo pila

---
+++ src/colas/AlumnoMain.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colas;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author chamb
+ */
+public class AlumnoMain {
+    public static void main(String[] args) {
+        Stack<Alumno> pila=new Stack<>();
+        
+        Alumno a=new Alumno("Ariel", "Fidelitas");
+        Alumno b= new Alumno("Juan", "Quispe");
+        Alumno c= new Alumno("Delia","Riva");
+        
+        pila.push(a);
+        pila.push(b);
+        pila.push(c);
+        
+        pila.pop();
+        System.out.println(pila.peek().getNombre());
+    }
+}

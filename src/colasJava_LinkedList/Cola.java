90dc865f2e429f5146b096bddc77a1d3c7029518
Erlinda <'erlindachambimanzanoerlinda@gamil.com'>
2020-03-05 11:51:54 -0400

    ejemplo colas java linkendList Queue

---
+++ src/colasJava_LinkedList/Cola.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colasJava_LinkedList;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+La cola en la cola del supermercado
+FIFO First In First Out
+Primero que entra, primero que sale
+ */
+public class Cola {
+    public static void main(String[] args) {
+        Queue<Integer> cola= new LinkedList<>();
+        
+        for (int i = 0; i < 10; i++) {
+            cola.offer(i);
+        }
+        cola.remove();//remueve el primero 0
+        System.out.println(cola.peek()); //muestra el primero
+    }
+}

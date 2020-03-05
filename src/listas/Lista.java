5f54ac1cefbac0d3c0dba9bc9487f01a90385f75
Erlinda <'erlindachambimanzanoerlinda@gamil.com'>
2020-03-05 11:49:17 -0400

    ejemplo listas con nodos makigas

---
+++ src/listas/Lista.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listas;
+
+/**
+ *
+ * @author chamb Makigas
+ */
+public class Lista {
+
+    private Nodo cabeza;
+    private int longitud = 0;
+
+    public void insertarPrincipio(Libro libro) {
+        Nodo nodo = new Nodo(libro);
+        nodo.siguiente = cabeza;
+        cabeza = nodo;
+        longitud++;
+    }
+
+    public void insertarFinal(Libro libro) {
+        Nodo nodo = new Nodo(libro);
+        Nodo puntero = cabeza;
+
+        while (puntero.siguiente != null) {
+            puntero = puntero.siguiente;
+        }
+        puntero.siguiente = nodo;
+        longitud++;
+    }
+
+    public void insertarDespues(int n, Libro libro) {
+        Nodo nodo = new Nodo(libro);
+        if (cabeza == null) {
+            cabeza = nodo;
+        } else {
+            Nodo puntero = cabeza;
+            int contador = 0;
+            while (contador < n && puntero.siguiente != null) {
+                puntero = puntero.siguiente;
+                contador++;
+            }
+            nodo.siguiente = puntero.siguiente;
+            puntero.siguiente = nodo;
+        }
+        longitud++;
+    }
+
+    public Libro obtener(int n) {
+        if (cabeza == null) {
+            return null;
+        } else {
+            Nodo puntero = cabeza;
+            int contador = 0;
+            while (contador < n && puntero.siguiente != null) {
+                puntero = puntero.siguiente;
+                contador++;
+            }
+            if (contador != n) {
+                return null;
+            } else {
+                return puntero.libro;
+            }
+        }
+    }
+
+    public int contar() {
+        return longitud;
+    }
+
+    public boolean estaVacia() {
+        return cabeza == null;
+    }
+
+    public void eliminarPrincipio() {
+        if (cabeza != null) {
+            Nodo primer = cabeza;
+            cabeza = cabeza.siguiente;
+            primer.siguiente = null;
+            longitud--;
+        }
+    }
+
+    public void eliminarUltimo() {
+        if (cabeza != null) {
+            if (cabeza.siguiente == null) {
+                cabeza = null;
+                longitud--;
+            } else {
+                Nodo puntero = cabeza;
+                while (puntero.siguiente.siguiente != null) {  //el nodo penultimo
+                    puntero = puntero.siguiente;
+                }
+                puntero.siguiente = null;
+                longitud--;
+            }
+        }
+    }
+
+    public void eliminarLibro(int n) {
+        if (cabeza != null) {
+            if (n == 0) {
+                Nodo primer = cabeza;
+                cabeza = cabeza.siguiente;
+                primer.siguiente = null;
+                longitud--;
+            } else if (n < longitud) {
+                Nodo puntero = cabeza;
+                int contador = 0;
+                while (contador < (n - 1)) { //para conectar el nodo anterior de n para conectar con el siguiente de n
+                    puntero = puntero.siguiente;
+                    contador++;
+                }
+                Nodo temp = puntero.siguiente;
+                puntero.siguiente = temp.siguiente;
+                temp.siguiente = null;
+                longitud--;
+            }
+        }
+    }
+
+    private class Nodo {
+
+        public Libro libro;
+        public Nodo siguiente = null;
+
+        public Nodo(Libro libro) {
+            this.libro = libro;
+        }
+    }
+}
